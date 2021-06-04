34cbc2644a549207bfc5664fbaadc963ec52fab1
irenenyatichi <irenenyatichik@gmail.com>
2021-06-04 16:02:34 +0300

    --- Auto-Git Commit ---

--- Loan.py
+++ Loan.py
@@ -1,10 +1,11 @@
 class Account:
-    def __init__ (self,name,phone,loan,loanLimit):
+    def __init__ (self,name,phone):
         self.phone = phone
         self.name = name
         self.balance = 0
-        self.loan = loan
+        self.loan = 0
         self.loanLimit = 1000
+
     def deposit(self,amount):
         if amount<=0:
             return f"The amount must be greater than zero"
@@ -21,4 +22,10 @@ class Account:
         else: 
             self.balance-=amount
             return f"Your balance is {self.balance}"
-    def borrow(self,loanLimit,amount):
+    def borrow(self,amount):
+        if amount<=self.loan_limit:
+            self.loan+=amount
+            self.balance+=self.loan
+            return f"Dear {self.name}, you have borrowed KES{amount}.Your loan is KES{self.loan}, your balance is KES{self.balance}"
+        else:
+            return f"Your loan request of KES{amount} is unsuccessful because your loan limit is KES{self.loan_limit}"
