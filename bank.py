d13993290be17bfa75c5c99ee9d9d43d495a6571
irenenyatichi <irenenyatichik@gmail.com>
2021-06-04 14:02:09 +0300

    --- Auto-Git Commit ---

--- bank.py
+++ bank.py
@@ -1,9 +1,12 @@
-class Bank:
-    def __init__ (self,name,number,type):
-        self.number = number
+class Account:
+    def __init__ (self,name,phone):
+        self.phone = phone
         self.name = name
-        self.type = type
-    def withdraw(self):
-        return f"{self.name} of {self.type} account, wishes to withdraw from account number {self.number} "
-    def deposit(self):
-        return f"{self.name} of {self.type} account, wishes to deposit from account number {self.number} "
+        self.balance = 0
+    # def withdraw(self):
+    #     return f"{self.name} of {self.type} account, wishes to withdraw from account number {self.number} "
+    def deposit(self,amount):
+        self.balance+=amount
+        return f"Dear {self.name} you have deposited {amount}. Your balance is {self.balance}"
+    def show_balance(self):
+        return self.balance
