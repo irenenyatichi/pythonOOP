7e9eed9a2983f5b5710ad901118cb49732505e2a
irenenyatichi <irenenyatichik@gmail.com>
2021-06-04 14:32:14 +0300

    --- Auto-Git Commit ---

--- bank.py
+++ bank.py
@@ -3,10 +3,19 @@ class Account:
         self.phone = phone
         self.name = name
         self.balance = 0
-    # def withdraw(self):
-    #     return f"{self.name} of {self.type} account, wishes to withdraw from account number {self.number} "
     def deposit(self,amount):
-        self.balance+=amount
-        return f"Dear {self.name} you have deposited {amount}. Your balance is {self.balance}"
+        if amount<=0:
+            return f"The amount must be greater than zero"
+        else:
+            self.balance+=amount
+            return f"Dear {self.name} you have deposited {amount}. Your balance is {self.balance}"
     def show_balance(self):
         return self.balance
+    def withdraw(self,amount):
+        if amount <=0:
+            return f"The amount must be greater than zero"
+        elif balance<amount:
+            return f"Dear {self.name} you have deposited {amount}. Your balance is {self.balance}"
+        else: 
+            self.balance-=amount
+            return f"Your balance is {self.balance}"
