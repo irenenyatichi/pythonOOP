8c3843119fa8863999d43dd4886b4e1aa5f04bf7
irenenyatichi <irenenyatichik@gmail.com>
2021-06-08 18:22:11 +0300

    --- Auto-Git Commit ---

--- bankLoan.py
+++ bankLoan.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+
 class Account:
-    def __init__ (self,name,phone):
+    def __init__ (self,name,phone,transactions):
         self.phone = phone
         self.name = name
         self.balance = 0
         self.loan = 0
         self.loanLimit = 1000
+        self.transactions = []
 
     def deposit(self,amount):
         if amount<=0:
             return f"The amount must be greater than zero"
         else:
             self.balance+=amount
+            transaction = {"amount":amount,"balance":self.balance,"time":datetime.now(),"narration":"Deposit"}
+            self.transactions.append(transaction)
             return f"Dear {self.name} you have deposited {amount}. Your balance is {self.balance}"
     def show_balance(self):
         return self.balance
@@ -23,7 +28,7 @@ class Account:
             self.balance-=amount
             return f"Your balance is {self.balance}"
     def borrow(self,amount):
-        if amount<=self.loan_limit:
+        if amount<=self.loanLimit:
             self.loan+=amount
             self.balance+=self.loan
             return f"Dear {self.name}, you have borrowed KES{amount}.Your loan is KES{self.loan}, your balance is KES{self.balance}"
